97bdcde22d7ca42abea3e5f19a5b280506415f86
Keita Kobayashi <keita@kbys.me>
2016-01-09 23:32:10 +0900

    Register new path of tempfile after it was moved during convert

--- lib/exfile_imagemagick/convert.ex
+++ lib/exfile_imagemagick/convert.ex
@@ -5,6 +5,7 @@ defmodule ExfileImagemagick.Convert do
 
   def call(file, [dest_format]) do
     image = coerce_file_to_tempfile(file, true) |> open |> format(dest_format)
+    Exfile.Tempfile.register_file(image.path)
     {:ok, {:tempfile, image.path}}
   end
 end
