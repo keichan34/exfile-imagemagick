b8a684645396d619a0ea16e402aa765dd1d737c4
Keita Kobayashi <keita@kbys.me>
2016-02-19 19:34:33 +0900

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExfileImagemagick.Mixfile do
   def project do
     [
       app: :exfile_imagemagick,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.2.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
