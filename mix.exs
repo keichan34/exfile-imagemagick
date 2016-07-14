c35a4a698d7d004469c4a78f1d334a651a6bca96
Keitaroh Kobayashi <keita@kbys.me>
2016-07-14 09:36:28 +0900

    Relax Elixir version constraint

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExfileImagemagick.Mixfile do
     [
       app: :exfile_imagemagick,
       version: "0.1.2",
-      elixir: "~> 1.2.0",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
